05c3cddefc582236eac7f902734b0358e4ac5fd5
Prashanth <prashanth.royal2046@gmail.com>
2021-05-27 08:19:49 +0530

    Added: add hotel

--- hotelReservation.java
+++ hotelReservation.java
@@ -1,1 +1,25 @@
-	#Welcome to Hotel Reservation System Problem
+
+public class hotelReservation {
+
+	 List<Hotel> hotelList = new ArrayList<Hotel>();
+	 
+	 public int addHotel(final String hotelName, final int cusRate) {
+		 hotelList.add(getHotel(hotelName, cusRate));
+		 return hotelList.size();
+	 }
+
+	 public Hotel getHotel(final String hotelName, final int cusRate) {
+		 Hotel hotel = new Hotel();
+		 hotel.setHotelName(hotelName);
+		 hotel.setRate(cusRate);
+		 return hotel;
+	 }
+	 
+	  
+	 public static void main(String[] args) {
+
+		 HRS hrs = new HRS();
+		 hrs.addHotel("Lakewood", 110);
+	}
+
+}
